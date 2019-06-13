f7979b24480e0793b3087a3504e25e771f3ad6eb
Andrii Zhulai <andrii.zhulai@yahoo.com>
2019-06-14 00:18:00 +0300

    commit all

--- UnitTestProject_Normal/UITests.cs
+++ UnitTestProject_Normal/UITests.cs
@@ -75,73 +75,73 @@ namespace UnitTestProject_Normal
             //}
 
 
-            [Test]
-            public void Findsome()
-            {
-                driver.FindElement(By.Id("twotabsearchtextbox"));
-                //Console.WriteLine("Search field is present on the page");
-                IWebElement incorerrectxpath = driver.FindElement(By.Id("nav-link-accountLis"));
+            //[Test]
+            //public void Findsome()
+            //{
+            //    driver.FindElement(By.Id("twotabsearchtextbox"));
+            //    //Console.WriteLine("Search field is present on the page");
+            //    IWebElement incorerrectxpath = driver.FindElement(By.Id("nav-link-accountLis"));
 
-                // try
-                //{
-                //    if (incorerrectxpath.Displayed)
-                //    {
-                //        GreenMessage("I see this element");
-                //    }
-                //}
+            //    // try
+            //    //{
+            //    //    if (incorerrectxpath.Displayed)
+            //    //    {
+            //    //        GreenMessage("I see this element");
+            //    //    }
+            //    //}
 
-                // catch (NoSuchElementException)
-                //{
-                //    RedMessage("I can not see this element");
-                //}
+            //    // catch (NoSuchElementException)
+            //    //{
+            //    //    RedMessage("I can not see this element");
+            //    //}
 
-                //if (incorerrectxpath.Displayed)
-                //{
-                //    GreenMessage("Hello, Andrii  ____ is present on the page");
-                //}
-                //else
-                //{
-                //    RedMessage("ERROR, Elemen_______is not dispalyed");
-                //}
+            //    //if (incorerrectxpath.Displayed)
+            //    //{
+            //    //    GreenMessage("Hello, Andrii  ____ is present on the page");
+            //    //}
+            //    //else
+            //    //{
+            //    //    RedMessage("ERROR, Elemen_______is not dispalyed");
+            //    //}
 
-            }
-
-
-            [Test]
-            public void Departments() //Check dropdown list
-            {
-                driver.FindElement(By.Id("twotabsearchtextbox")).SendKeys("lebron james shoes");
-                driver.FindElement(By.CssSelector("#nav-search > form > div.nav-right > div > input")).Click();
-                //screenshot
-                driver.FindElement(By.Id("twotabsearchtextbox")).SendKeys("rqwqwrwqrqwrqwrwqrqw");
-                driver.FindElement(By.CssSelector("#nav-search > form > div.nav-right > div > input")).Click();
-                // driver.FindElement(By.XPath("//*[@id=\"search\"]/div[1]/div[2]/div/span[2]/div/span[1]")).Text;
-            }
+            //}
 
 
-            [Test]
-            public void Dropdown()
-            {
-                Actions action = new Actions(driver);
-                IWebElement elem = driver.FindElement(By.LinkText("Departments"));
-                action.MoveToElement(elem).Perform();
+            //[Test]
+            //public void Departments() //Check dropdown list
+            //{
+            //    driver.FindElement(By.Id("twotabsearchtextbox")).SendKeys("lebron james shoes");
+            //    driver.FindElement(By.CssSelector("#nav-search > form > div.nav-right > div > input")).Click();
+            //    //screenshot
+            //    driver.FindElement(By.Id("twotabsearchtextbox")).SendKeys("rqwqwrwqrqwrqwrwqrqw");
+            //    driver.FindElement(By.CssSelector("#nav-search > form > div.nav-right > div > input")).Click();
+            //    // driver.FindElement(By.XPath("//*[@id=\"search\"]/div[1]/div[2]/div/span[2]/div/span[1]")).Text;
+            //}
 
-                //driver.FindElements(By.XPath("//*[@id=\"nav - flyout - shopAll\"]")).Count();
 
-                //ReadOnlyCollection<IWebElement> elements = driver.FindElements(By.XPath("//*[@id=\"nav-link-shopall\"]/span[2]"));
-                //Console.WriteLine(elements.Count);
-                //foreach(var item in elements)
-                //{
-                //    Console.WriteLine(item.Text);
-                //}
-                ReadOnlyCollection<IWebElement> elements = driver.FindElements(By.TagName("span"));
-                //tyt mozhna vukorustatu by tagname s vzyatu po  tegu napruclad h2
-                Console.WriteLine(elements.Count);
-                foreach (var item in elements)
-                {
-                    Console.WriteLine(item.Text);
-                }          
-            }
+            //[Test]
+            //public void Dropdown()
+            //{
+            //    Actions action = new Actions(driver);
+            //    IWebElement elem = driver.FindElement(By.LinkText("Departments"));
+            //    action.MoveToElement(elem).Perform();
+
+            //    //driver.FindElements(By.XPath("//*[@id=\"nav - flyout - shopAll\"]")).Count();
+
+            //    //ReadOnlyCollection<IWebElement> elements = driver.FindElements(By.XPath("//*[@id=\"nav-link-shopall\"]/span[2]"));
+            //    //Console.WriteLine(elements.Count);
+            //    //foreach(var item in elements)
+            //    //{
+            //    //    Console.WriteLine(item.Text);
+            //    //}
+            //    ReadOnlyCollection<IWebElement> elements = driver.FindElements(By.TagName("span"));
+            //    //tyt mozhna vukorustatu by tagname s vzyatu po  tegu napruclad h2
+            //    Console.WriteLine(elements.Count);
+            //    foreach (var item in elements)
+            //    {
+            //        Console.WriteLine(item.Text);
+            //    }          
+            //}
 
             [Test]
             public void Cart()
@@ -178,25 +178,25 @@ namespace UnitTestProject_Normal
 
             }
 
-            [Test]
-            public void Newtest()
-            {
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-                driver.FindElement(By.LinkText("Departments"));
-                driver.FindElement(By.LinkText("Browsing History"));
-                driver.FindElement(By.LinkText("Andrii's Amazon.com"));
-                driver.FindElement(By.LinkText("Today's Deals"));
-                driver.FindElement(By.LinkText("Gift Cards"));
-                driver.FindElement(By.LinkText("Help"));
-                driver.FindElement(By.LinkText("Registry"));
-                Console.WriteLine("All links are present on the page");
+            //[Test]
+            //public void Newtest()
+            //{
+            //    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            //    driver.FindElement(By.LinkText("Departments"));
+            //    driver.FindElement(By.LinkText("Browsing History"));
+            //    driver.FindElement(By.LinkText("Andrii's Amazon.com"));
+            //    driver.FindElement(By.LinkText("Today's Deals"));
+            //    driver.FindElement(By.LinkText("Gift Cards"));
+            //    driver.FindElement(By.LinkText("Help"));
+            //    driver.FindElement(By.LinkText("Registry"));
+            //    Console.WriteLine("All links are present on the page");
               
 
                
 
 
 
-            }
+            //}
 
 
 
